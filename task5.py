4c1a08cc6eb6368ae0ea6ab84a921cabdd3580a3
Lynxtail <l.gorbachev@mail.ru>
2022-04-08 12:58:53 +0400

    need fix task 5

--- task5.py
+++ task5.py
@@ -2,75 +2,99 @@
 
 import numpy as np
 
+
+def get_mu(c_opt, L):
+    mu = [.01] * L
+    h = 0.01
+    index = 0
+    lambda_ans = 0.
+    lambda_ans_past = -1
+    while lambda_ans < c_opt and lambda_ans > lambda_ans_past:
+        # !!!!
+        # заменить на перераспределение мю
+        mu[index % L] += h
+        index += 1
+        lambda_ans_past = lambda_ans
+        # пропускная способность сети
+        *_, lmbds = recurrent_method(mu, N, theta, L)
+        lambda_ans = sum(lmbds)
+        print(f'mu = {mu}')
+        print(f'Lambda_old = {lambda_ans_past}, Lambda_new = {lambda_ans}')
+    return lambda_ans, mu
+
 def stationary_distribution(omega, theta, eps):
     while np.linalg.norm(omega.dot(theta) - omega) > eps:
         omega = omega.dot(theta)
     return omega
 
-# нормализующие константы
-def get_G(L, N, x):
-    G = np.zeros((N+1, L))
-    for i in range(N + 1):
-        G[i][0] = x[0]**i
-    G[0] = [1]*L    
-    for i in range(1, N + 1):
-        for j in range(1, L):
-            G[i][j] = G[i][j-1] + x[j] * G[i-1][j]
-    return G
-
-# м.о. длительности пребывания требований в подсети N (S_1 - S_5)
-def get_tau():
+def recurrent_method(mu, Q, theta, L):
     # нахождение вектора omega
-    omega = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0])
+    omega = np.zeros(L)
+    omega[0] = 1
     eps = 0.0001
     omega = stationary_distribution(omega, theta, eps)
-    print(f'Omegas: {omega},\nCheck (~1): {sum(omega)}')
-
-    x = omega / mu 
-    G = get_G(L, N, x)
-    # вероятности что в системах m и более требований
-    p = np.zeros((N+1, L))
-    # вероятности что в системах ровно m требований
-    p_m = np.zeros((N+1,L))
+    # print(f'Omegas: {omega},\nCheck (~1): {sum(omega)}')
+    
     # м.о. числа требований в системах
-    s = np.zeros(L)
+    s = np.zeros((Q+1, L))
+    # м.о. длительности пребывания требований в системах
+    u = np.zeros((Q+1, L))
+    # м.о. длительности ожидания требований в очереди системы
+    w = np.zeros(L)
+    # м.о. числа требований, ожидающих обслуживание в очереди системы
+    b = np.zeros(L)
     # м.о. числа занятых приборов в системах
     h = np.zeros(L)
-    # интенсивности входного потока требований в системах
+    # интенсивность входящего потока требований в системы
     lmbds = np.zeros(L)
-    # м.о. длительности пребывания требований в системах
-    u = np.zeros(L)
+    
+    # м.о. длительности пребывания требований в системах и м.о. числа требований в системах
+    for Y in range(1, Q + 1):
+        for i in range(L - 1):
+            u[Y][i] = (1 / mu[i]) * (s[Y-1][i] + 1)
+        for i in range(L - 1):
+            summa = 0
+            for j in range(L):
+                summa += omega[j] * u[Y][j]
+            s[Y][i] = omega[i] * u[Y][i] * Y / summa
 
-    for i in range(N + 1):
-        for j in range(L):
-            p[i][j] = x[j]**i * (G[N-i][L-1] / G[N][L-1])
-
-    for i in range(N + 1):
-        for j in range(L):
-            p_m[i][j] = (x[j]**i / G[N][L-1]) * (G[N-i][L-1] - x[j] * G[N-i-1][L-1])
-
-    for i in range(L):
-        for j in range(1, N + 1):
-            s[i] += x[i]**j * (G[N-j][L-1] / G[N][L-1])
-        h[i] = x[i] * (G[N-1][L-1] / G[N][L-1])
+    # вычисление остальных характеристик сети
+    for i in range(L - 1):
+        w[i] = u[Q][i] - (1 / mu[i])
+        b[i] = s[Q][i] * w[i] / u[Q][i]
+        h[i] = s[Q][i] - b[i]
         lmbds[i] = h[i] * mu[i]
-        u[i] = s[i] / lmbds[i]
 
-    # математическое ожидание длительности пребывания требований в системах обслуживания
-    print('\n\nМ.о. длительности пребывания требований в системах:\n')
-    for i in range(L):
-        print(f'Система {i + 1}: {u[i]}')
+    return omega, u, w, b, s, lmbds
+
+# м.о. длительности пребывания требований в подсети N (S_1 - S_5)
+def get_tau(lmbds, u, L, lambda_L):
+    for i in range(L - 1):
+        tau += lmbds[i] * u[i]
+    tau /= lambda_[L]
+
+# м.о. числа требований в подсети N (S_1 - S_5)
+def get_s_N(lmbds, s, L):
+    for i in range(L - 1):
+        s_N += lmbds[i] * s[i]
+    # s_N /= lmbds[L]
+
+def get_lambda_L():
+    lambda_L = [0] * L
+    for item in lambda_L:
+        while get_tau(lmbds, u, L, item) <= T:
+            # подбор значения lambda_6 для каждого k = 1, ... 10
 
 L = 6 # 5 смо + 1 управляющая
 N = 10 # число требований в сети
-с = 0
+с = 0 # предельное значение lambda_6
 
 theta = np.array([
-                [0, .2, .3, .4, .1],
-                [.3, 0, .1, .2, .4],
-                [.2, .2, 0, .3, .3],
-                [.3, .2, .2, 0, .3],
-                [.1, .4, .4, .1, 0]])
+                [0, .5, .5, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1],
+                [0, .7, 0, 0, .3, 0],
+                [.2, 0, 0, 0, 0, .8],
+                [0, 0, 1, 0, 0, 0]])
 
 mu = [1, 1, 1, 1, 1]
 T = 4
